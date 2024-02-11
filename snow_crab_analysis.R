07578cbbc0592156a96e1a2ba5e4ca35b4cca1ea
CopperBeech <jospbarss@gmail.com>
2024-02-11 11:05:30 -0400

    Add male percentage

--- snow_crab_analysis.R
+++ snow_crab_analysis.R
@@ -11,6 +11,11 @@ sex_ratio_func <- function(m, f){
   ratio[ratio %in% c(NaN, Inf, 0)] <- NA
   return(ratio)
 }
+male_pct_function <- function(m, f){
+  total = m+f
+  m_pct <- m/total
+  return(m_pct)
+}
 # Simpson diversity index function (might want to replace with diversity from vegan)
 simpson_index_func <- function(species_mat){
   species_mat <- as.matrix(species_mat)
@@ -27,12 +32,17 @@ simpson_index_func <- function(species_mat){
 crab_data <- crab_data %>% 
   mutate(snow_crab_sex_ratio = 
            sex_ratio_func(snow_crab_males, snow_crab_females),
+         male_percentage = male_pct_function(snow_crab_males, snow_crab_females),
          Simpson_index = 
-           simpson_index_func(crab_data[, 9:19]))
+           simpson_index_func(crab_data[, 9:19]),
+         Simpson_index_2 <- diversity(crab_data[, 9:19]))
 # Make a few plots
 ggplot(crab_data) +
   geom_histogram(aes(x=snow_crab_sex_ratio))+
   labs(title = "Snow Crab Sex Ratio")
+ggplot(crab_data) +
+  geom_histogram(aes(x=male_percentage))+
+  labs(title = "Snow Crab Male Percentage")
 ggplot(crab_data) +
   geom_histogram(aes(x=Simpson_index))+
   labs(title = "Simpson Index")
