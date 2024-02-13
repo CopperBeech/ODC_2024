4b3c33c38be6c9d3c1a334c55eac0383fce7f0a6
CopperBeech <jospbarss@gmail.com>
2024-02-12 22:45:45 -0400

    App now working well

--- crab_app/app.R
+++ crab_app/app.R
@@ -2,38 +2,80 @@
 
 library(shiny)
 library(tidyverse)
+library(leaflet)
 
 # Define UI for application that draws histograms
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Snow Crab Trawl Survey Data"),
-    
-    sidebarLayout(
-        sidebarPanel(
-            selectInput("variable",
-                        "Choose variable to plot:",
-                        list("Total snow crabs" = "total_crabs",
-                             "Male snow crabs" = "male",
-                             "Female snow crabs" = "female",
-                             "Proportion of female snow crabs" = "female_percentage",
-                             "Simpson diversity index" = "Simpson_index")
-                        )
-        ),
+  titlePanel("Snow Crab Trawl Survey Data"),
+  fluidRow(
+    column(3, offset = 0.5,
+           selectInput("variable",
+                       "Choose variable to plot:",
+                       list("Total snow crabs",
+                            "Male snow crabs",
+                            "Female snow crabs",
+                            "Proportion of female snow crabs",
+                            "Simpson diversity index")
+             )
+           )
+    ),
 
         # Show a plot of the generated distribution
-        mainPanel(
+  fluidRow(
+    column(5, offset = 0.5,
            plotOutput("distPlot")
-        )
+           ),
+    column(7,
+           leafletOutput("map")
+           )
     )
 )
 
 # Define server logic required to draw histograms
 server <- function(input, output) {
-  crab_data <- read_csv("crab_data_clean_for_app.csv")
-    output$distPlot <- renderPlot({
-      ggplot(crab_data)+
-        geom_histogram(aes_string(x=input$variable))
+  
+  crab_data <- read_csv("crab_data_clean_for_app.csv") %>% 
+    rename("Total snow crabs" = "total_crabs",
+           "Male snow crabs" = "male",
+           "Female snow crabs" = "female",
+           "Proportion of female snow crabs" = "female_percentage",
+           "Simpson diversity index" = "Simpson_index")
+  
+  app_data <- reactive({
+    crab_data %>% 
+      select(lon, lat, input$variable)
+    })
+
+  output$distPlot <- renderPlot({
+    plot_data <- req(app_data())
+    var <- as.character(input$variable)
+    colnames(plot_data)[3] <- var
+    ggplot(plot_data)+
+      geom_histogram(aes(x = .data[[var]]))+
+      ggtitle(paste("Histogram of", var))+
+      theme_light()
+    })
+  
+  output$map <- renderLeaflet({
+    map_dat <- req(app_data())
+    colnames(map_dat)[3] <- "chosen_var"
+    if(input$variable %in% c("Total snow crabs",
+                             "Male snow crabs",
+                             "Female snow crabs")){
+      scale_dat <- map_dat %>% 
+        filter(chosen_var < quantile(chosen_var, 0.99, na.rm = TRUE))
+    } else{
+      scale_dat <- map_dat
+    }
+    pal <- colorNumeric("Purples", domain = scale_dat$chosen_var)
+      m <- leaflet(map_dat) %>%
+        addTiles() %>%
+        addCircleMarkers(~lon, ~lat, radius = 1, 
+                         color = ~pal(chosen_var)) %>% 
+        addLegend("bottomleft", pal = pal, map_dat$chosen_var, 
+                  title = input$variable)
     })
 }
 
