3c951c69eb57f107df2bac345bf5607f1c777850
CopperBeech <jospbarss@gmail.com>
2024-02-17 13:47:00 -0400

    Cleaned crab_app code

--- crab_app/app.R
+++ crab_app/app.R
@@ -4,10 +4,8 @@ library(shiny)
 library(tidyverse)
 library(leaflet)
 
-# Define UI for application that draws histograms
 ui <- fluidPage(
-
-    # Application title
+  
   titlePanel("Snow Crab Trawl Survey Data"),
   fluidRow(
     column(3, offset = 0.5,
@@ -15,9 +13,9 @@ ui <- fluidPage(
                        "Choose year",
                        list("All years",
                             2019, 2020, 2021, 2022)
-                       )
-           ),
-    column(3,
+           )
+    ),
+    column(4,
            selectInput("variable",
                        "Choose variable to plot:",
                        list("Total snow crabs",
@@ -25,25 +23,23 @@ ui <- fluidPage(
                             "Female snow crabs",
                             "Proportion of female snow crabs",
                             "Simpson diversity index")
-             )
            )
-    ),
-
-        # Show a plot of the generated distribution
+    )
+  ),
+  
   fluidRow(
     column(5, offset = 0.5,
            plotOutput("distPlot")
-           ),
+    ),
     column(7,
            leafletOutput("map")
-           )
     )
+  )
 )
 
-# Define server logic required to draw histograms
 server <- function(input, output) {
   
-  crab_data <- read_csv("crab_data_clean_for_app.csv") %>% 
+  crab_data <- readRDS("crab_data_clean.rds") %>% 
     rename("Total snow crabs" = "total_crabs",
            "Male snow crabs" = "male",
            "Female snow crabs" = "female",
@@ -53,13 +49,31 @@ server <- function(input, output) {
   
   
   app_data <- reactive({
-      if(input$year != "All years"){
-        crab_data <- filter(crab_data, year == input$year)
-      } else crab_data <- crab_data
-      crab_data %>% 
-        select(lon, lat, input$variable)
-    })
-
+    if(input$year != "All years"){
+      crab_data <- filter(crab_data, year == input$year)
+    } else crab_data <- crab_data
+    crab_data %>% 
+      select(lon, lat, input$variable)
+  })
+  
+  scale_dat <- reactive({
+    scale_dat <- req(app_data())
+    if(input$variable %in% c("Total snow crabs",
+                             "Male snow crabs",
+                             "Female snow crabs")){
+      scale_dat <- scale_dat %>%
+        filter(.[[3]] < quantile(.[[3]], 0.999, na.rm = TRUE))
+    } else{
+      scale_dat <- scale_dat
+    }
+  })
+  
+  
+  colorpal <- reactive({
+    col_dat <- req(scale_dat())
+    colorpal <- colorNumeric("Purples", domain = col_dat[,3])
+  })
+  
   output$distPlot <- renderPlot({
     plot_data <- req(app_data())
     var <- as.character(input$variable)
@@ -68,29 +82,30 @@ server <- function(input, output) {
       geom_histogram(aes(x = .data[[var]]))+
       ggtitle(paste("Histogram of", var))+
       theme_light()
-    })
+  })
   
   output$map <- renderLeaflet({
+    leaflet(crab_data) %>%
+      addProviderTiles(providers$Esri.OceanBasemap) %>% 
+      fitBounds(~min(lon), ~min(lat), ~max(lon), ~max(lat))
+  })
+  
+  observe({
+    pal <- colorpal()
     map_dat <- req(app_data())
-    colnames(map_dat)[3] <- "chosen_var"
-    if(input$variable %in% c("Total snow crabs",
-                             "Male snow crabs",
-                             "Female snow crabs")){
-      scale_dat <- map_dat %>% 
-        filter(chosen_var < quantile(chosen_var, 0.99, na.rm = TRUE))
-    } else{
-      scale_dat <- map_dat
-    }
-    pal <- colorNumeric("Purples", domain = scale_dat$chosen_var)
+    colnames(map_dat) <- c("lon", "lat", "chosen_var")
+    leg_dat <- req(scale_dat())
+    colnames(leg_dat) <- c("lon", "lat", "chosen_var_leg")
     lab <- paste0(input$variable, ": ", map_dat$chosen_var)
-      m <- leaflet(map_dat) %>%
-        addProviderTiles(providers$Esri.OceanBasemap) %>%
-        addCircleMarkers(~lon, ~lat, radius = 1, 
-                         color = ~pal(chosen_var), label = lab) %>% 
-        addLegend("bottomleft", pal = pal, map_dat$chosen_var, 
-                  title = input$variable)
-    })
+    leafletProxy("map", data = map_dat) %>%
+      clearMarkers() %>% 
+      clearControls() %>% 
+      addCircleMarkers(~lon, ~lat, radius = 1,
+                       color = ~pal(chosen_var), label = lab) %>%
+      addLegend("topright", pal = pal, leg_dat$chosen_var_leg,
+                title = input$variable)
+  })
+  
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
